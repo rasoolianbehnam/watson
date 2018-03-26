9c6d45ffd14d31fd5281f24a9a4180d56e9942a7
rasoolianbehnam <behnam.rasoulian@gmail.com>
2018-03-26 18:12:54 -0500

    modified manuscript

---
+++ papers/manuscripts/manuscript/internals.pl
@@ -0,0 +1,130 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_milenkovic2010cancer/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations_all/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:correlation/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_milenkoviae2008uncovering/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_prvzulj2007biological/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:distance_total/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances_intra/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:graphlet_extraction/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/local_thresholded_chr1_chr1/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h2a/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h2b/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h1/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphlet_correlation_schema/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_intra/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_di2010fast/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphlet_distance_schema/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_adhikari2016chromosome3d/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_cancer_intra/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit_distances_intra_cancer/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:mic/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_intra_cancer/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances_all/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_all/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lieberman2009comprehensive/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:distance_single/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_reshef2011detecting/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations_intra/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:pearson/;
+$ref_files{$key} = "$dir".q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ papers/manuscripts/manuscript/labels.pl
@@ -0,0 +1,141 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:orbit_distances_intra_cancer/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:mic/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_intra_cancer/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances_all/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_all/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_reshef2011detecting/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:distance_single/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lieberman2009comprehensive/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations_intra/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:pearson/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_milenkovic2010cancer/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations_all/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_milenkoviae2008uncovering/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:correlation/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:distance_total/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_prvzulj2007biological/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances_intra/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbit-distances/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:contact_maps_correlations/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:graphlet_extraction/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/local_thresholded_chr1_chr1/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h2a/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h2b/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:h1/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphlet_correlation_schema/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_correlations_intra/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_di2010fast/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/graphlet_distance_schema/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_adhikari2016chromosome3d/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:orbits_cancer_intra/;
+$external_labels{$key} = "$URL/" . q|manuscript.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+1;
+
