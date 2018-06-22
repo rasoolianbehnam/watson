374c249bcafd26ff763f557e566eccbce3afd137
rasoolianbehnam <behnam.rasoulian@gmail.com>
2018-06-22 11:08:06 -0500

    cleaned up a little bit

--- orbits/rscript.r
+++
@@ -1,8 +0,0 @@
-library('orca')
-for (i in 0:72) {
-    filename = sprintf("orbit%02d.txt", i);
-    a <- read.csv(filename, header=FALSE); 
-    b = count5(a); 
-    c = sum(b[1, ] > 0); 
-    print(c);
-}
